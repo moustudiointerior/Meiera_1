2174b42aa0b0531f539eaaf19b64646e25ed9251
moustudiointerior <144911346+moustudiointerior@users.noreply.github.com>
2024-01-24 15:29:12 +0100

    Initial commit

---
+++ data.js
@@ -0,0 +1,363 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-korytarz-1",
+      "name": "korytarz 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 750,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.756194294266386,
+          "pitch": 0.0926602520242632,
+          "rotation": 0,
+          "target": "1-korytarz-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-korytarz-2",
+      "name": "korytarz 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 750,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.4620581772751642,
+          "pitch": 0.0641524553961954,
+          "rotation": 0,
+          "target": "7-salon-1"
+        },
+        {
+          "yaw": -0.0423458844535709,
+          "pitch": 0.04666134776196884,
+          "rotation": 0,
+          "target": "5-azienka-maa-1"
+        },
+        {
+          "yaw": 2.6773768141033205,
+          "pitch": 0.016549457103254284,
+          "rotation": 0,
+          "target": "4-azienka-dua-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-kuchnia-1",
+      "name": "kuchnia 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 750,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.32500530038037745,
+          "pitch": 0.14872067418737878,
+          "rotation": 0,
+          "target": "3-kuchnia-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-kuchnia-2",
+      "name": "kuchnia 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 750,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.1915216705381724,
+          "pitch": 0.0911715542804572,
+          "rotation": 0,
+          "target": "8-salon-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-azienka-dua-1",
+      "name": "łazienka duża 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 750,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.39986167685446,
+          "pitch": 0.06597042398563246,
+          "rotation": 0,
+          "target": "9-azienka-dua-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-azienka-maa-1",
+      "name": "łazienka mała 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 750,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.7528203740402297,
+          "pitch": 0.059098779008392555,
+          "rotation": 0,
+          "target": "6-azienka-maa-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-azienka-maa-2",
+      "name": "łazienka mała 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 750,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.44220171821887355,
+          "pitch": -0.003021060352359939,
+          "rotation": 0,
+          "target": "1-korytarz-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-salon-1",
+      "name": "salon 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 750,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.06752365606004318,
+          "pitch": 0.019109230817964118,
+          "rotation": 0,
+          "target": "2-kuchnia-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-salon-2",
+      "name": "salon 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 750,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.28562658487862436,
+          "pitch": 0.05455882419432001,
+          "rotation": 0,
+          "target": "1-korytarz-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-azienka-dua-2",
+      "name": "łazienka duża 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 750,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.4480150385430157,
+          "pitch": 0.0728165400358769,
+          "rotation": 0,
+          "target": "0-korytarz-1"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
